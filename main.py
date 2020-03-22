045d2992d4250bcac595ac8df140074644d161b1
Pedro Blandim <pedro.blandim@gmail.com>
2020-03-22 01:29:57 -0300

    add cpu monitoring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import socket
 import telepot
+import psutil
 
 TOKEN = '1113352697:AAG5giiZwUq-TQJwcDZNApYvRe5rfsiyDNA'
 #GROUP_ID = 643741476
@@ -12,19 +13,72 @@ def getIp():
     s.close()
     return ip
 
-def handle(bot, upId, ip):
-    while True:
-        up = bot.getUpdates()[-1]
+def handle(bot, ip, request):
+    # Possible requests:
+    # ip
+    # cpu <intervals=1> <each=False>
 
-        if upId != up['update_id']: # new msg
-            upId = up['update_id']
-            if up['message']['text'].lower() == 'ip':
-                bot.sendMessage(GROUP_ID, ip)
+    request =  request['message']['text'].lower().split(' ')
+    command =  request[0]  
+    print(f'req: {request} com:{command}')
+    
+
+    # ip 
+    if command == 'ip':
+        response = ip
+       
+
+    # cpu usage
+    elif command == 'cpu':
+        try: 
+            intervals = int(request[1]) if int(request[1]) > 0 else 1
+        except:
+            intervals = 1 # default value, in case intervals is not given
+        
+        cpus = len(psutil.cpu_percent(percpu=True)) # number of cores
+        response = "CPU USAGE IN %\n\n"
+        try:
+            each = request[2]
+        except:
+            each = False
+
+        if each:
+            for i in range(intervals):
+                usage = psutil.cpu_percent(interval=0.1, percpu=True)
+                for i in range(cpus):
+                    if i == cpus//2:
+                        response += '\n'
+                    response += f'Cpu{i}: {usage[i]}   '
+                response += '\n\n'
+
+        else:
+            for i in range(intervals):
+                usage = psutil.cpu_percent(interval=0.1)
+                response += f'Cpu: {usage}\n'
+        
+    bot.sendMessage(GROUP_ID, response)
+    return response
+        
+
+
+
+
+def listen(bot, ip):
+
+    upId = bot.getUpdates()[-1]['update_id']
+    while True:
+        lastUp = bot.getUpdates()[-1] # last update
+
+        # upId saves the id of the last update answered, except when initializing
+        # if the last update answered is equal to the last update, there's nothing to be done
+        if upId != lastUp['update_id']: # new msg
+            upId = lastUp['update_id']
+            handle(bot, ip, lastUp)
+
 
 
 if __name__ == "__main__":
     bot = telepot.Bot(TOKEN)
     ip = getIp()[0]
-    upId = 0
-    handle(bot, upId, ip)
+    listen(bot, ip)
 
